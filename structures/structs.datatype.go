491755d4b784c345af47e49cffcd8b4ce0e6df2a
felipe goncalves <felipe.goncalves@semantix.ai>
2024-05-17 18:05:13 -0300

    docs: struct

--- structures/structs.datatype.go
+++ structures/structs.datatype.go
@@ -2,6 +2,8 @@ package structures
 
 import "fmt"
 
+// structs are like objects in Javascript, you can use it for join diferent data types
+// it's possible to use for build a user, for example
 type Person struct {
 	name string
 	age int
@@ -11,5 +13,9 @@ type Person struct {
 
 func WhatTheStruct() {
 	person := Person{name: "Felipe", age: 18, handsome:true}
+	/*
+	this is one possibility to implement a struct, but you can also ommit the keys, just passing the values, like that:
+	persont := Person{"John", "30", false}
+	*/
 	fmt.Println(person)
 }
